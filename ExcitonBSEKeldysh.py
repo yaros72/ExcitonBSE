f558e51ba5a17844ab7c6bad629e4a6d4bc463eb
yaros72 <yaroslav.zhumagulov@gmail.com>
2019-05-28 14:50:55 +0300

    add Triom

--- ExcitonBSEKeldysh.py
+++ ExcitonBSEKeldysh.py
@@ -1,42 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import product
-import numba
 import tbmodels
-from scipy.special import struve
-from scipy.special import yn
+from scipy.special import struve,yn
 import os
 from scipy.linalg import eigh
+from ase.units import *
 import h5py
-
-@numba.njit(parallel=True,nogil=True,fastmath=True)
-def FastExcitonHamiltonian(H,E,shift,D,D_,W,cb,vb,Q,indexes):
-    NH=H.shape[0];N=E.shape[0]
-    for indx1 in numba.prange(NH):
-        kx1,ky1,c1,v1=indexes[indx1]
-       
-        H[indx1,indx1]+=E[(kx1+Q[0])%N,(ky1+Q[1])%N,c1]-E[kx1,ky1,v1]+shift
-        
-        for indx2 in range(indx1,NH):
-            kx2,ky2,c2,v2=indexes[indx2]
-            
-            O12=np.dot(D_[(kx1+Q[0])%N,(ky1+Q[1])%N].T,D[(kx2+Q[0])%N,(ky2+Q[1])%N]);
-            O21=np.dot(D_[kx2,ky2].T,D[kx1,ky1]);
-            H[indx1,indx2]-=W[(kx1-kx2)%N,(ky1-ky2)%N]*O12[c1,c2]*O21[v2,v1]
-            #exchange
-            
-            O11=np.dot(D_[(kx1+Q[0])%N,(ky1+Q[1])%N].T,D[kx1,ky1]);
-            O22=np.dot(D_[kx2,ky2].T,D[(kx2+Q[0])%N,(ky2+Q[1])%N]);
-            H[indx1,indx2]+=W[Q[0],Q[1]]*O11[c1,v1]*O22[v2,c2]
-            
-            H[indx2,indx1]=H[indx1,indx2]
-                                    
-                                 
-    return H
+from fast_functions import *
+from slow_functions import *
 
 class ExcitonMoS2(object):
 
-    def __init__(self,N,vb,cb,epsilon,r0=33.875,shift=0.5,spin_orbit=True):
+    def __init__(self,N,vb,cb,epsilon,
+                 r0=33.875,shift=0.5,spin_orbit=True,cutoff=np.inf):
         if spin_orbit:
             self.model =tbmodels.Model.from_wannier_files(
                                                             hr_file='TB-Models/MoS2_hr.dat',
@@ -55,8 +32,8 @@ class ExcitonMoS2(object):
         self.k2=self.model.reciprocal_lattice[1]
         self.a1=self.model.uc[0]
         self.a2=self.model.uc[1]
-        self.V=np.sqrt(3)/2*self.a1[0]**2
         self.norb=self.model.hamilton([0.,0.,0.]).shape[0]
+        self.cutoff=cutoff
         
         self.epsilon=epsilon #
         self.r0= r0/epsilon 
@@ -65,8 +42,7 @@ class ExcitonMoS2(object):
         self.vb=vb # valence bands
         self.cb=cb # conduction bands
         self.nv=len(self.vb);self.nc=len(self.cb)
-        self.NH=self.N**2*self.nc*self.nv
-        print('Exciton Hamiltonian size: '+str(self.NH)+'X'+str(self.NH))
+        
         
 
         self.E=np.zeros((N,N,self.norb));self.K=np.zeros((N,N,3));
@@ -77,28 +53,143 @@ class ExcitonMoS2(object):
             e,d=np.linalg.eigh(self.H[i,j])
             self.E[i,j]=e;self.D[i,j]=d
             self.K[i,j]=i*self.k1/N+j*self.k2/N
-        self.E_gap=np.min(self.E[:,:,cb[0]]-self.E[:,:,vb[-1]])+shift
 
         R=np.zeros((N,N,3))
         for i,j in product(range(N),range(N)):
             R[i,j]=self.a1*(i-N/2)+self.a2*(j-N/2)
         self.R=R
-        WR=np.zeros((N,N))
+        WR=np.zeros((N,N));VR=np.zeros((N,N))
         for i,j in product(range(N),range(N)):
             radius=np.linalg.norm(R[i,j])
-            if radius!=0:WR[i,j]=14.4*np.pi/(2*self.epsilon*self.r0)*(struve(0,radius/self.r0)-yn(0,radius/self.r0))
-            else:WR[i,j]=0
+            if radius!=0:
+                WR[i,j]=Hartree*Bohr*np.pi/(2*self.epsilon*self.r0)*(struve(0,radius/self.r0)-yn(0,radius/self.r0))
+                VR[i,j]=Hartree*Bohr/radius
+            else:
+                WR[i,j]=0
+                VR[i,j]=0
             
 
         WR=np.fft.fftshift(WR);self.W=np.fft.fft2(WR)/N**2
-        self.indexes=[(kx,ky,i,j) for kx,ky,i,j in product(range(self.N),range(self.N),self.cb,self.vb)]
+        VR=np.fft.fftshift(VR);self.V=np.fft.fft2(VR)/N**2
         
+        self.gap=self.E[:,:,cb[0]]-self.E[:,:,vb[-1]]+shift
+        self.Egap=np.min(self.gap)
+        self.weight=np.ones((self.N,self.N))
+        
+        self.weight[self.gap>(self.Egap+self.cutoff)]=0
+        self.indexes=[]
+        for kx,ky in product(range(self.N),range(self.N)):
+            if self.weight[kx,ky]==1:
+                for i,j in product(self.cb,self.vb):
+                    self.indexes.append((kx,ky,i,j))
+        self.NH=len(self.indexes)
+        print('Exciton Hamiltonian size: '+str(self.NH)+' K-space size: '+str(int(np.sum(self.weight))))
+        
+  
+        R=np.fft.fftshift(self.R,axes=(0,1))
+        HR=np.fft.ifft2(self.H,axes=(0,1))
+        dx=np.fft.fft2(1j*R[:,:,0,None,None]*HR,axes=(0,1))
+        dy=np.fft.fft2(1j*R[:,:,1,None,None]*HR,axes=(0,1))
+        for i,j in product(range(N),range(N)):
+            dx[i,j]=np.linalg.multi_dot([self.D[i,j].T.conj(),dx[i,j],self.D[i,j]])
+            dy[i,j]=np.linalg.multi_dot([self.D[i,j].T.conj(),dy[i,j],self.D[i,j]])
+        self.dx=dx;self.dy=dy
+    
+    def constructTrionBasis(self,Trion_Q=[(1./3.),(1./3.),0]):
+        self.Trion_Q=np.array([q*self.N for q in Trion_Q],dtype=int)
+        self.trion_indexes=[]
+        for kx1,ky1,kx2,ky2 in product(range(self.N),range(self.N),range(self.N),range(self.N)):
+            if self.weight[kx1,ky1]+self.weight[kx2,ky2]==2:
+                kx3=(kx1+kx2-self.Trion_Q[0])%self.N
+                ky3=(ky1+ky2-self.Trion_Q[1])%self.N
+                if self.weight[kx3,ky3]==1:
+                    for i1,i2,i3 in product(self.cb,self.cb,self.vb):
+                        self.trion_indexes.append((kx1,ky1,
+                                                   kx2,ky2,
+                                                   kx3,ky3,
+                                                   i1,i2,i3))
+        self.NT=len(self.trion_indexes)
+        print('Trion Hamiltonian size: '+str(self.NT)+' K-space size: '+str(int(np.sum(self.weight))))
+        
+    def constructTrionHamiltonian(self):
+        HT=np.zeros((self.NT,self.NT),dtype=complex)
+        self.HT=FastTrionHamiltonian(HT,self.E,self.D,self.W,self.V,self.trion_indexes,self.shift)
 
-    def plotBandStructure(self,emax=5,emin=-8,E_Fermi=0):
+    def constuctExcitonHamiltonian(self,Q=[0,0,0],optic=True):
+        self.Q=np.array([q*self.N for q in Q],dtype=int)
+        HH=np.empty((self.NH,self.NH),dtype=np.complex)
+        self.HH=FastExcitonHamiltonian(HH,self.E,self.shift,self.D,self.W,self.V,self.indexes,self.Q)
+    
+    def calculateAbsobtionSpectrumTrion(self,eta=0.03,omega_max=5,omega_n=50000,n_iter=300):
+        omega=np.linspace(0,omega_max,omega_n+1)+1j*eta
+        omega=np.delete(omega,0)
+        P=np.zeros(self.NT,dtype=complex)
+        for i in range(self.NT):
+            x1,y1,x2,y2,x3,y3,i1,i2,i3=self.trion_indexes[i]
+            if x1==self.Trion_Q[0] and y1==self.Trion_Q[1]:
+                if x2==x3 and y2==y3:
+                    P[i]+=self.dx[x2,y2,i2,i3]
+            if x2==self.Trion_Q[0] and y2==self.Trion_Q[1]:
+                if x1==x3 and y1==y3:
+                    P[i]-=self.dx[x1,y1,i1,i3]
+                    
+        a,b=lanczos(self.HT,P,n_iter)
+        eps=np.zeros(omega.size,dtype=complex)
+        for i in range(1,n_iter):
+            eps=b[-i]**2/(omega+1j*eta-a[-i]-eps)
+        eps=1/(omega+1j*eta-a[0]-eps)
+        self.trion_eps=eps
+        self.trion_omega=omega
+    
+    def calculateAbsobtionSpectrum(self,eta=0.03,omega_max=5,omega_n=5000,n_iter=300):     
+        omega=np.linspace(0,omega_max,omega_n+1)+1j*eta
+        omega=np.delete(omega,0) 
+        
+        P=np.array([self.dx[indx] for indx in self.indexes])
+        a,b=lanczos(self.HH,P,n_iter)
+            
+        eps=np.zeros(omega.size,dtype=complex)
+        for i in range(1,n_iter):
+            eps=b[-i]**2/(omega+1j*eta-a[-i]-eps)
+        eps=1/(omega+1j*eta-a[0]-eps)
+        self.eps=eps
+        self.omega=omega
+        
+    def plotAbsobtionSpectrum(self,shift):
+        plt.figure()
+        if hasattr(self, 'omega') and hasattr(self, 'eps'):
+            plt.plot(self.omega.real,-self.eps.imag,label='Exciton')
+            
+        if hasattr(self, 'trion_omega') and hasattr(self, 'trion_eps'):
+            plt.plot(self.trion_omega.real-shift,-self.trion_eps.imag,label='Trion')
+        plt.grid()
+        plt.legend()
+        plt.xlim([0,None])
+        
+    
+    def plotExcitonWaveFunction(self,i):
+        wave_k,wave_r=self.ExcitonWaveFunction(i)
+
+        plt.figure()
+        plt.subplot(211, title="Reciprocal Space")
+        plt.scatter(self.K[:,:,0],self.K[:,:,1],c=wave_k)
+        plt.xlabel('$\AA^{-1}$');plt.ylabel('$\AA^{-1}$')
+        plt.grid()
+        plt.axis('equal')
+            
+        plt.subplot(212, title="Real Space")
+        plt.scatter(self.R[:,:,0],self.R[:,:,1],c=wave_r)
+        plt.xlabel('$\AA$');plt.ylabel('$\AA$')
+        plt.grid()
+        plt.axis('equal')
+        
+        plt.suptitle('Peak: '+str(np.round(self.EH[i],4)))
+        
+    def plotBandStructure(self,N1=20,emax=5,emin=-8,E_Fermi=0):
         gamma=np.array([0.,0.,0.])
         M=np.array([0,0.5,0.])
-        K=np.array([0.33,0.33,0.])
-        kpoints=[];N1=20;N2=int(N1/2);N3=int(np.sqrt(N1**2+N2**2))
+        K=np.array([1./3.,1./3.,0.])
+        kpoints=[];N1=N1;N2=int(N1/2);N3=int(np.sqrt(N1**2+N2**2))
         for i in range(N1):
             kpoints.append(i*M/N1+(N1-i)*gamma/N1)
         for i in range(N2):
@@ -123,88 +214,3 @@ class ExcitonMoS2(object):
         plt.xticks([0,N1,N1+N2,N1+N2+N3-1],['$\Gamma$','M','K','$\Gamma$'])
         plt.xlim([0.,N1+N2+N3-1])
         plt.ylim([emin,emax])
-
-    def constuctExcitonHamiltonian(self,Q=[0,0,0]):
-        self.Q=Q
-        HH=np.zeros((self.NH,self.NH),dtype=np.complex)
-        print('Construction Begin!')
-        self.HH=FastExcitonHamiltonian(HH,self.E,self.shift,self.D,self.D.conj(),self.W,self.cb,self.vb,self.Q,self.indexes)
-        print('Construction Done!')
-        
-    def solveExcitonHamiltonian(self,save=False):
-        
-        self.EH,self.DH=eigh(self.HH)
-        self.DH=self.DH[:,np.argsort(self.EH)]
-        self.EH=np.sort(self.EH)
-        print('Hamiltonian Solved!')
-
-        if save:
-            if not os.path.exists('save'):
-                os.makedirs('save')
-            print('Saving Eigenvalues and Eigenfunctions...')
-            
-            h5f = h5py.File('save/'+str(self.Q)+'.h5', 'w')
-            h5f.create_dataset('E', data=self.EH)
-#             h5f.create_dataset('H', data=self.HH)
-            h5f.create_dataset('D', data=self.DH)
-            h5f.close()
-            
-            print('Saved!')
-
-    def ExcitonWaveFunction(self,i,type='rho'):
-        
-        wave_k=self.DH[:,i].reshape(self.N,self.N,self.nc,self.nv)
-        wave_k=np.sum(wave_k,axis=(2,3))
-        wave_k/=np.sqrt(np.sum(wave_k.conj()*wave_k))
-        
-        wave_r=np.fft.fftshift(np.fft.fft2(wave_k))/self.N**2
-        
-        rho_k=wave_k.conj()*wave_k
-        rho_r=wave_r.conj()*wave_r
-        if type=='rho':
-            return rho_k.real,rho_r.real
-        elif type=='wave':
-            return wave_k,wave_r
-    
-    def absobtionSpectrum(self,theta_array=[0],eta=0.03,omega_max=5,omega_n=5000):
-        
-        plt.figure()
-        for theta in theta_array:
-            P1=np.gradient(self.H,axis=0);P2=np.gradient(self.H,axis=1)
-            Px=(np.sqrt(3)/2.*P1);Py=(P2+P1/2.)
-            self.P=np.cos(theta)*Px+np.sin(theta)*Py
-            for i in range(self.N):
-                for j in range(self.N):
-                    self.P[i,j]=np.linalg.multi_dot([self.D[i,j].conj().T,self.P[i,j],self.D[i,j]])
-            P=np.array([self.P[indx] for indx in self.indexes])
-            optical=np.zeros(self.NH)
-            for i in range(self.NH):
-                optical[i]=np.abs(np.sum(P*self.DH[:,i]))**2
-            omega=np.linspace(0,omega_max,omega_n+1);omega=np.delete(omega,0)
-            absorbtion=np.zeros(omega.size,dtype=np.complex)
-            for i in range(omega.size):
-                absorbtion[i]=np.sum(optical/(omega[i]+1j*eta-self.EH))/omega[i]
-            spectrum=-absorbtion.imag/np.pi
-            spectrum/=np.trapz(spectrum,omega)
-
-            plt.plot(omega,spectrum)
-        plt.grid()
-    
-    def plotExcitonWaveFunction(self,i):
-        wave_k,wave_r=self.ExcitonWaveFunction(i)
-
-        plt.figure()
-        plt.subplot(211, title="Reciprocal Space")
-        plt.scatter(self.K[:,:,0],self.K[:,:,1],c=wave_k)
-        plt.xlabel('$\AA^{-1}$');plt.ylabel('$\AA^{-1}$')
-        plt.grid()
-        plt.axis('equal')
-            
-        plt.subplot(212, title="Real Space")
-        plt.scatter(self.R[:,:,0],self.R[:,:,1],c=wave_r)
-        plt.xlabel('$\AA$');plt.ylabel('$\AA$')
-        plt.grid()
-        plt.axis('equal')
-        
-        plt.suptitle('Peak: '+str(np.round(self.EH[i],4)))
-        
---
+++ slow_functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def lanczos(H,P,n_iter):
+    a=[];b=[];vec=[]
+    P/=np.linalg.norm(P)
+    vec.append(P)
+    a.append(vec[-1].conj()@H@vec[-1])
+    vec.append(H@vec[-1]-a[-1]*vec[-1])
+    vec[-1]/=np.linalg.norm(vec[-1])
+    b.append(vec[-2].conj()@H@vec[-1])
+
+    for n in range(1,n_iter):
+        a.append(vec[-1].conj()@H@vec[-1])
+        b.append(vec[-2].conj()@H@vec[-1])
+        vec.append(H@vec[-1]-a[-1]*vec[-1]-b[-1]*vec[-2])
+        vec[-1]/=np.abs(np.linalg.norm(vec[-1]))
+        del vec[0]
+    return a,b
